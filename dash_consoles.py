18daf98c41697ec5622077f77f970c34a484ee50
Vinicius Schilieve Santos <47587017844@info.local>
2024-09-19 21:55:44 -0300

    att

--- dash_consoles.py
+++ dash_consoles.py
@@ -13,16 +13,16 @@ app = Dash(__name__)
 df  = pd.read_excel("vendas_consoles_atualizada.xlsx")
 # Esta linha le o arquivo do Excel e armazena os dados em uma variavel chamada df
 
-fig = px.bar(df,x="Empresa", y="Console", color = "Empresa", barmode="group")
+fig = px.bar_polar(df,x="Console", y="Vendas (milhões)", color = "Empresa", barmode="group")
 
 opcoes = list(df['Empresa'].unique())
 #Essa linha adiciona a struing todas as lojas ao final da lista de opcoes 
 opcoes.append("Todas as Empresas")
 
 app.layout = html.Div(children=[
-    html.H1(children='Faturamento das Lojas'),
-    html.H2(children='Gráfico com o faturamento de todos os produtos separados por loja'),
-    dcc.Dropdown(opcoes, value= 'Todas as lojas', id='lista_lojas'),
+    html.H1(children='Faturamento das Empresas'),
+    html.H2(children='Gráfico com o faturamento de todos os consoles separados por empresas'),
+    dcc.Dropdown(opcoes, value= 'Todas as Empresas', id='lista_empresa'),
 
     dcc.Graph(
         id='grafico_quantidade_produto',
@@ -35,15 +35,15 @@ app.layout = html.Div(children=[
 
 @app.callback(
     Output('grafico_quantidade_produto','figure'),
-    Input('lista_lojas','value')
+    Input('lista_empresa','value')
 )
 
 def update_output(value):
-    if value == 'Todas as Lojas':
-        fig = px.bar(df, x="Produto",y="Quantidade", color ="ID Loja", barmode="group")
+    if value == 'Todas as Empresas':
+        fig = px.bar(df, x="Console",y="Vendas (milhões)", color ="Empresa", barmode="group")
     else:
-        tabela_filtrada =df.loc[df['ID Loja'] == value, :]
-        fig = px.bar(tabela_filtrada, x="Produto",y="Quantidade", color ="ID Loja", barmode="group")
+        tabela_filtrada =df.loc[df['Empresa'] == value, :]
+        fig = px.bar(tabela_filtrada, x="Console",y="Vendas (milhões)", color ="Ano de Lançamento", barmode="group")
     return fig 
 
 
